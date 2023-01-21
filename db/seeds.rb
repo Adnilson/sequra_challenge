ec908073ef69c20f493e41debdf4eb9f8ae81a0b
Adnilson Delgado <adnilson.delgado@globalapptesting.com>
2023-01-21 22:13:11 +0000

    Fix a bug when Order records where not created because shoppers external key was absent

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,14 @@ merchants = File.read("db/seed-files/merchants.json")
 orders = File.read("db/seed-files/orders.json")
 shoppers = File.read("db/seed-files/shoppers.json")
 
+JSON.parse(shoppers)["RECORDS"].each do |shopper|
+  Shopper.create(shopper)
+end
+
 JSON.parse(merchants)["RECORDS"].each do |merchant|
   Merchant.create(merchant)
 end
 
 JSON.parse(orders)["RECORDS"].each do |order|
   Order.create(order)
-end
-
-JSON.parse(shoppers)["RECORDS"].each do |shopper|
-  Shopper.create(shopper)
 end
